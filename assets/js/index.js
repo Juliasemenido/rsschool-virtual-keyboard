4fb15456d2b4ca453708364cfc49d86a3ffba3b1
Juliasemenido <semenido.1997@gmail.com>
2022-05-05 12:42:15 +0300

    init: add skeletone

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+let createAndAppend = function({className, parentElement, value}, tag = 'div') {
+    let element = document.createElement(tag);
+    element.className = className;
+    if (value) {
+       element.innerHTML = value;
+    }
+    parentElement.appendChild(element);
+
+    return element;
+};
